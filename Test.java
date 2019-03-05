eeee14a8aeb205e86b11a3365a7da818f0a41ec9
João Victor <joao.coutinho9@gmail.com>
2019-03-05 13:12:37 -0300

    Base

--- Test.java
+++ Test.java
@@ -12,4 +12,17 @@ class Test {
         int d = 1;
     }
 
+    public void deletionBothAndAdditionBoth() {
+        int x = 0;
+        int y = 1;
+        int z = 0;
+        int w = 1;
+    }
+
+    public void deletionAndAdditionBoth() {
+        int x = 0;
+        int y = 1;
+        int z = 0;
+    }
+
 }
