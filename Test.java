977f50ba88706324f9d151ca017f394d0a2d93a2
João Victor <joao.coutinho9@gmail.com>
2019-03-05 12:26:32 -0300

    Left

--- Test.java
+++ Test.java
@@ -2,13 +2,11 @@ class Test {
 
     public void deletionSingleAndAddition() {
         int x = 0;
-        int y = 1;
         int z = 0;
     }
 
     public void deletionBothAndAddition() {
         int x = 0;
-        int y = 1;
         int z = 0;
     }
 
